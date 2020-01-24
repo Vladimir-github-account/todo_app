914c2785b329f5fd1d8d05aa01d7cc15dd01be40
User Name <artur.it2914@gmail.com>
2020-01-24 14:01:34 +0200

    Create: sequelize setup

---
+++ server/src/db/config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    username: 'arthur',
+    password: 'admin',
+    database: 'fd_todo_db',
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres'
+  },
+  test: {},
+  production: {},
+};
---
+++ server/src/db/models/user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define( 'User', {
+    firstName: {
+      type: DataTypes.STRING( 64 ),
+      is: /[A-Z][a-z]*/,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING( 64 ),
+      is: /[A-Z][a-z]*/,
+      allowNull: false
+    },
+    login: {
+      type: DataTypes.STRING,
+      is: /^[^ ^()*&?|\\/]{6,16}$/,
+      allowNull: false,
+    },
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      isEmail: true,
+    }
+  }, {} );
+  User.associate = function (models) {
+    // associations can be defined here
+  };
+  return User;
+};

f22b1db894d603933c7b67ca4b2645354fc5313c
User Name <artur.it2914@gmail.com>
2020-01-28 16:47:30 +0200

    Create: user validation schemas

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -2,11 +2,12 @@ import { User } from './../db/models';
 
 export async function createUser (req, res, next) {
   try {
-    const createdUser = await User.create( req.body );
+    const createdUser = await User.create( req.body, {} );
     if (createdUser) {
-      return res.status( 201 ).send( createdUser );
+      const data = createdUser.get();
+      delete data.password;
+      return res.status( 201 ).send( data );
     }
-
   } catch (e) {
     next( e );
   }

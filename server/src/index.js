9db302b2ec9ae1eec4e5033be5da0c3195d7a827
User Name <artur.it2914@gmail.com>
2020-01-28 15:09:52 +0200

    Add: user router

--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,14 @@
-import express  from 'express';
-import { User } from './db/models';
+import express from 'express';
+
+const cors = require( 'cors' );
+import router  from './routes';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use( express.json() );
+app.use( cors() );
+app.use( express.json() );//Content-type: application/json
 
-app.get( '/', (req, res) => res.send( 'Hello World!' ) );
-
-app.post( '/user', async (req, res, next) => {
-            try {
-              const createdUser = await User.create( req.body );
-
-              res.send( createdUser );
-            } catch (e) {
-              next( e );
-            }
-          }
-);
-
-app.use( function (err, req, res, next) {
-  console.error( err.stack );
-  res.status( 500 ).send( 'Something broke!' );
-} );
+app.use( router );
 
 app.listen( PORT, () => console.log( `Example app listening on port ${PORT}!` ) );

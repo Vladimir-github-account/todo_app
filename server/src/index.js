995404dd5b1d1687ebbe6c06b43626bbd0d22df2
User Name <artur.it2914@gmail.com>
2020-01-28 15:36:33 +0200

    Add: user controller

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,8 @@
-import express from 'express';
+import express      from 'express';
 
 const cors = require( 'cors' );
-import router  from './routes';
+import router       from './routes';
+import errorHandler from './middlewares/errorHandler.js';
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,5 +11,5 @@ app.use( cors() );
 app.use( express.json() );//Content-type: application/json
 
 app.use( router );
-
+app.use( errorHandler );
 app.listen( PORT, () => console.log( `Example app listening on port ${PORT}!` ) );

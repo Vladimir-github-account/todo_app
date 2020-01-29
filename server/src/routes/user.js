d28c6c5fb866e2edce0a50f8de79a0c7ac17319e
User Name <artur.it2914@gmail.com>
2020-01-29 15:44:17 +0200

    Refactor: user validation middleware

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,13 +1,12 @@
-import express        from 'express';
-import { createUser } from '../controllers/user.controller.js';
-import validateUser   from '../middlewares/user/validateUser.js';
+import express                      from 'express';
+import { createUser }               from '../controllers/user.controller.js';
+import { validateUserDataOnCreate } from '../middlewares/user/validateUser.js';
 
 const userRouter = express.Router();
 
 userRouter.post( '/',
-                 validateUser,
-                 createUser );
-
-
+                 validateUserDataOnCreate,
+                 createUser
+);
 
 export default userRouter;

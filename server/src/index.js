de0bbfb160bf2b0754ba4c18bcf4774911bcb033
User Name <artur.it2914@gmail.com>
2020-01-30 15:35:23 +0200

    Create: express error handlers

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,44 @@
-import express      from 'express';
+import express                 from 'express';
+import cors                    from 'cors' ;
+import router                  from './routes';
+import applicationErrorHandler from './middlewares/error_handlers/applicationErrorHandler.js';
+import sequelizeErrorHandler   from './middlewares/error_handlers/sequelizeErrorHandler.js';
 
-const cors = require( 'cors' );
-import router       from './routes';
-import errorHandler from './middlewares/errorHandler.js';
-
+/*
+ * defining server port
+ * */
 const PORT = process.env.PORT || 5000;
 
+/*
+ * create server instance
+ * */
 const app = express();
-app.use( cors() );
-app.use( express.json() );//Content-type: application/json
 
+/*
+ * set cors headers
+ * */
+app.use( cors() );
+/*
+ * allows Content-type: application/json
+ * */
+app.use( express.json() );
+
+/*
+ * routing
+ * */
 app.use( router );
-app.use( errorHandler );
+
+/*
+ * error handling
+ * */
+
+app.use( applicationErrorHandler );
+app.use( sequelizeErrorHandler );
+app.use( (err, req, res) => {
+  return res.status( 500 ).send( 'Internal server error' );
+} );
+
+/*
+ * start listening
+ * */
 app.listen( PORT, () => console.log( `Example app listening on port ${PORT}!` ) );

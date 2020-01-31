7599ae304082802e0ab9eca10761b0459d9c6640
User Name <artur.it2914@gmail.com>
2020-01-31 14:03:15 +0200

    Refactor: validationErrorHandler.js

--- server/src/middlewares/error_handlers/validationErrorHandler.js
+++ server/src/middlewares/error_handlers/validationErrorHandler.js
@@ -5,26 +5,11 @@ export default (err, req, res, next) => {
   if (err instanceof ValidationError) {
     const { details: [errorDetails] } = err;
 
-    let errorMessage = '';
     const valueLabel = errorDetails.context.label || 'Property';
-    switch (errorDetails.type) {
-      case 'any.required': {
 
-        errorMessage = `${valueLabel} is required`;
-      }
-        break;
-      case 'any.invalid':
-        errorMessage = `${valueLabel} value is not valid.`;
-        break;
-      case 'string.pattern.base':
-        errorMessage = `${valueLabel} with value \"${errorDetails.context.value}\" fails to match the required pattern.`;
-        break;
-      default:
-        errorMessage = 'Validation failed';
-        break;
-    }
-
-    return res.status( 400 ).send( errorMessage );
+    return res.status( 400 ).send( errorDetails.type === 'string.pattern.base'
+                                   ? `${valueLabel} with value \"${errorDetails.context.value}\" fails to match the required pattern.`
+                                   : errorDetails.message );
 
   }
   next( err );

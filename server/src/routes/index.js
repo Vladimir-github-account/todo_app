7d939d7d3717243bdeeb5ffd1c050d4f1666ca37
User Name <artur.it2914@gmail.com>
2020-01-31 14:06:57 +0200

    Refactor: authorization MW

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,10 @@
-import express       from 'express';
-import userRouter    from './user.js';
-import authorizeUser from '../middlewares/authorization/authorizeUser.js';
+import express                from 'express';
+import userRouter             from './user.js';
+import checkUserAuthorization from '../middlewares/authorization/checkUserAuthorization.js';
 
 const router = express.Router();
 
-router.use( '*', authorizeUser );
+router.use( checkUserAuthorization );
 
 router.use( '/user', userRouter );
 

220b4c5bb5d83785babaf8529bfac5db934e6b13
User Name <artur.it2914@gmail.com>
2020-02-03 15:30:03 +0200

    Create: task validation schema

--- server/src/utils/data_validations/task.js
+++ server/src/utils/data_validations/task.js
@@ -1,9 +1,9 @@
-import Joi     from '@hapi/joi';
-import JoiDate from '@hapi/joi-date';
-
-Joi.extend( JoiDate );
+import Joi from '@hapi/joi';
 
 export default Joi.object( {
-
-                           } );
+                             value: Joi.string().trim().min( 1 ).max( 255 ).label( 'Value' ).required(
+                               '$isCreateMode' ),
+                             isDone: Joi.boolean().optional().label( 'Done' ),
+                             deadline: Joi.date().greater( 'now' ).label( 'Deadline' ).required( '$isCreateMode' ),
+                           } ).min( 1 ).max( 3 );
 
